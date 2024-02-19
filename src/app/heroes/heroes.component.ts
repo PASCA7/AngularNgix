c7d773b21cd8d68e15225835aad1d43d8ff17cf9
PASCA7 <9890015099y@gmail.com>
2024-02-19 17:14:52 +0530

    test commit to my Account 9890015099y@gmail.com

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { Hero } from '../heroes';
+@Component({
+  selector: 'app-heroes',
+  standalone: true,
+  imports: [],
+  templateUrl: './heroes.component.html',
+  styleUrl: './heroes.component.scss'
+})
+export class HeroesComponent {
+  hero: Hero = {
+    id: 1,
+    name: 'Windstorm'
+  };
+}
